61821a58422fb349ed12677fa73abe2a7112a443
erajneeshverma <rajneeshverma94306@gmail.com>
2023-11-13 23:43:39 +0530

    Loading component implemeted

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import authService from "./appwrite/auth"
 import {login, logout} from "./store/authSlice"
 import { Footer, Header } from './components'
 import { Outlet } from 'react-router-dom'
+import LoadingComponent from './pages/LoadingComponent'
 
 function App() {
   const [loading, setLoading] = useState(true)
@@ -22,17 +23,19 @@ function App() {
     .finally(() => setLoading(false))
   }, [])
   
-  return !loading ? (
-    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
-      <div className='w-full block'>
-        <Header />
-        <main>
-        <Outlet />
-        </main>
-        <Footer />
-      </div>
+  return (
+    <div>
+      {loading ? (<LoadingComponent />): (<div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+        <div className='w-full block'>
+          <Header />
+          <main>
+          <Outlet />
+          </main>
+          <Footer />
+        </div>
+      </div>)}
     </div>
-  ) : null
+  ) 
 }
 
 export default App
