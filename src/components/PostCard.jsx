8d4693e008b4a206397d288734586afcc54a639e
erajneeshverma <rajneeshverma94306@gmail.com>
2023-11-14 23:44:49 +0530

    Logo Modified

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import appwriteService from "../appwrite/config"
 import {Link} from 'react-router-dom'
 
 function PostCard({$id, title, featuredImage}) {
-    
+
   return (
     <Link to={`/post/${$id}`}>
         <div className='w-full bg-gray-100 rounded-xl p-4'>
